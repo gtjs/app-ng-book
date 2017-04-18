4b4018f7b37f7344c6c57de6af1c66ac645d2b43
Diego Chavez <diego@volumens.com>
2017-04-17 23:03:21 -0600

    Added assets

---
+++ src/app/meetup.ts
@@ -0,0 +1,43 @@
+export default function users(){
+  return [
+ {
+   "name": "Juan Merida"
+ },
+ {
+   "name": "Diego Porras"
+ },
+ {
+   "name": "Haroldo López"
+ },
+ {
+   "name": "Jose Morejon"
+ },
+ {
+   "name": "Erick Brayan Suy Miranda"
+ },
+ {
+   "name": "Daniel Con Rustrian"
+ },
+ {
+   "name": "Martinez Gómez Paublo"
+ },
+ {
+   "name": "Dayton Estuardo Garcia Montufar"
+ },
+ {
+   "name": "Paublo Alejandro Martinez Gómez"
+ },
+ {
+   "name": "Cristian ramirez"
+ },
+ {
+   "name": "Rodrigo Chanclan"
+ },
+ {
+   "name": "Kenneth Rangel"
+ },
+ {
+   "name": "Leonardo Cobón Montenegro"
+ }
+];
+}
