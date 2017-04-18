9d76795717b1514b42acad5d6a241d18938cb636
Diego Chavez <diego@volumens.com>
2017-04-17 23:00:07 -0600

    Small App ngBook Giveaway

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Users from './meetup'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  users = Users();
+  title = 'Ganador del ngBook v4!';
+  winner = {name:"Esperando ganardor.."}
+  button = 'Obtener Ganador!'
+  getWinner(){
+    let min = 0;
+    let max = this.users.length
+    let offset = min;
+    let range = (max - min) + 1;
+    let randomNumber = Math.floor( Math.random() * range) + offset;
+    this.button = 'buscando al ganador...';
+    setTimeout(()=>{
+       this.button = 'Felicidades :D';
+       this.winner = this.users[randomNumber];
+    },3000)
+    
+  }
 }
